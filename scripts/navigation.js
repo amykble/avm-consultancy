85c3eadf035400526d169056fe7c4ce02623ef4e
amykble <56142077+amykble@users.noreply.github.com>
2020-01-13 14:46:19 +0000

    added intersection observer to nav

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -9,3 +9,26 @@ function check() {
         spanColor.style.backgroundColor = "#2E2E2E";
   }
 }
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".landing-page");
+
+const sectionOneOptions = {
+    rootMargin: "-75% 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, sectionOneObserver){
+    
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                header.classList.add("nav-scrolled");
+            } else {
+                header.classList.remove("nav-scrolled");
+            }
+        });
+
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
